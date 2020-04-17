2ef8d2bc9eb4274b8669c723c5303e8c91e040b1
Sergi Navarro <serginavarro.ro@gmail.co>
2020-04-17 15:14:23 +0100

    Second commit
    Built only

---
+++ build/precache-manifest.f5a7f299c46f94d75e401ea35ebd8f39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0c2064cc9108c7cf075261c9f185cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23850fccb8681dbb980",
+    "url": "/static/css/main.708e877a.chunk.css"
+  },
+  {
+    "revision": "8b68e1b9c5771df0b400",
+    "url": "/static/js/2.bbd67d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bbd67d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23850fccb8681dbb980",
+    "url": "/static/js/main.61644a58.chunk.js"
+  },
+  {
+    "revision": "bf3583e12e030a75ff57",
+    "url": "/static/js/runtime-main.8e983008.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
